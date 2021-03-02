8feca413cce408708ebb6d71658d6621704e459a
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-03-02 16:07:32 -0300

    changes in streaming modules

--- src/sync/streaming/SSEHandler/NotificationKeeper.ts
+++ src/sync/streaming/SSEHandler/NotificationKeeper.ts
@@ -1,24 +1,36 @@
-import { ControlType, PUSH_CONNECT, PUSH_DISABLED, PUSH_DISCONNECT } from '../constants';
+import { ControlType, PUSH_SUBSYSTEM_UP, PUSH_NONRETRYABLE_ERROR, PUSH_SUBSYSTEM_DOWN } from '../constants';
 import { IPushEventEmitter } from '../types';
 
-const CONTROL_PRI_CHANNEL_REGEX = /control_pri$/;
+const CONTROL_CHANNEL_REGEXS = [/control_pri$/, /control_sec$/];
 
 /**
  * Factory of notification keeper, which process OCCUPANCY and CONTROL notifications and emits the corresponding push events.
  *
  * @param pushEmitter emitter for events related to streaming support
  */
-// @TODO update logic to handle OCCUPANCY for any region and rename according to new spec (e.g.: PUSH_CONNECT --> PUSH_SUBSYSTEM_UP)
+// @TODO update logic to handle OCCUPANCY for any region and rename according to new spec (e.g.: PUSH_SUBSYSTEM_UP --> PUSH_SUBSYSTEM_UP)
 export default function notificationKeeperFactory(pushEmitter: IPushEventEmitter) {
 
-  let occupancyTimestamp = -1;
-  let hasPublishers = true; // false if the number of publishers is equal to 0 in the last OCCUPANCY notification from CHANNEL_PRI
-  let controlTimestamp = -1;
-  let hasResumed = true; // false if last CONTROL event was STREAMING_PAUSED or STREAMING_DISABLED
+  let channels = CONTROL_CHANNEL_REGEXS.map(regex => ({
+    regex,
+    hasPublishers: true, // keep track of publishers presence per channel, in order to compute `hasPublishers`. Init with true, to emit PUSH_SUBSYSTEM_UP if initial OCCUPANCY notifications have 0 publishers
+    oTime: -1, // keep track of most recent occupancy notification timestamp per channel
+    cTime: -1 // keep track of most recent control notification timestamp per channel
+  }));
+
+  // false if the number of publishers is equal to 0 in all regions
+  let hasPublishers = true;
+
+  // false if last CONTROL event was STREAMING_PAUSED or STREAMING_DISABLED
+  let hasResumed = true;
+
+  function getHasPublishers() { // computes the value of `hasPublishers`
+    return channels.some(c => c.hasPublishers);
+  }
 
   return {
     handleOpen() {
-      pushEmitter.emit(PUSH_CONNECT);
+      pushEmitter.emit(PUSH_SUBSYSTEM_UP);
     },
 
     isStreamingUp() {
@@ -26,36 +38,50 @@ export default function notificationKeeperFactory(pushEmitter: IPushEventEmitter
     },
 
     handleOccupancyEvent(publishers: number, channel: string, timestamp: number) {
-      if (CONTROL_PRI_CHANNEL_REGEX.test(channel) && timestamp > occupancyTimestamp) {
-        occupancyTimestamp = timestamp;
-        if (hasResumed) {
-          if (publishers === 0 && hasPublishers) {
-            pushEmitter.emit(PUSH_DISCONNECT); // notify(STREAMING_DOWN) in spec
-          } else if (publishers !== 0 && !hasPublishers) {
-            pushEmitter.emit(PUSH_CONNECT); // notify(STREAMING_UP) in spec
+      for (let i = 0; i < channels.length; i++) {
+        const c = channels[i];
+        if (c.regex.test(channel)) {
+          if (timestamp > c.oTime) {
+            c.oTime = timestamp;
+            c.hasPublishers = publishers !== 0;
+            const newHasPublishers = getHasPublishers();
+            if (hasResumed) {
+              if (!newHasPublishers && hasPublishers) {
+                pushEmitter.emit(PUSH_SUBSYSTEM_DOWN);
+              } else if (newHasPublishers && !hasPublishers) {
+                pushEmitter.emit(PUSH_SUBSYSTEM_UP);
+              }
+              // nothing to do when hasResumed === false:
+              // streaming is already down for `!newHasPublishers`, and cannot be up for `newHasPublishers`
+            }
+            hasPublishers = newHasPublishers;
           }
-          // nothing to do when hasResumed === false:
-          // streaming is already down for `publishers === 0`, and cannot be up for `publishers !== 0`
+          return;
         }
-        hasPublishers = publishers !== 0;
       }
     },
 
     handleControlEvent(controlType: ControlType, channel: string, timestamp: number) {
-      if (CONTROL_PRI_CHANNEL_REGEX.test(channel) && timestamp > controlTimestamp) {
-        controlTimestamp = timestamp;
-        if (controlType === ControlType.STREAMING_DISABLED) {
-          pushEmitter.emit(PUSH_DISABLED);
-        } else if (hasPublishers) {
-          if (controlType === ControlType.STREAMING_PAUSED && hasResumed) {
-            pushEmitter.emit(PUSH_DISCONNECT); // notify(STREAMING_DOWN) in spec
-          } else if (controlType === ControlType.STREAMING_RESUMED && !hasResumed) {
-            pushEmitter.emit(PUSH_CONNECT); // notify(STREAMING_UP) in spec
+      for (let i = 0; i < channels.length; i++) {
+        const c = channels[i];
+        if (c.regex.test(channel)) {
+          if (timestamp > c.cTime) {
+            c.cTime = timestamp;
+            if (controlType === ControlType.STREAMING_DISABLED) {
+              pushEmitter.emit(PUSH_NONRETRYABLE_ERROR);
+            } else if (hasPublishers) {
+              if (controlType === ControlType.STREAMING_PAUSED && hasResumed) {
+                pushEmitter.emit(PUSH_SUBSYSTEM_DOWN);
+              } else if (controlType === ControlType.STREAMING_RESUMED && !hasResumed) {
+                pushEmitter.emit(PUSH_SUBSYSTEM_UP);
+              }
+              // nothing to do when hasPublishers === false:
+              // streaming is already down for `STREAMING_PAUSED`, and cannot be up for `STREAMING_RESUMED`
+            }
+            hasResumed = controlType === ControlType.STREAMING_RESUMED;
           }
-          // nothing to do when hasPublishers === false:
-          // streaming is already down for `STREAMING_PAUSED`, and cannot be up for `STREAMING_RESUMED`
+          return;
         }
-        hasResumed = controlType === ControlType.STREAMING_RESUMED;
       }
     },
 
