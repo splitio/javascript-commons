3426f1dd561d17f9012632f57c669e42e690006c
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-03-26 12:44:31 -0300

    cleanup imports

--- src/logger/messages/error.ts
+++ src/logger/messages/error.ts
@@ -1,33 +1,33 @@
-import { ERROR_ENGINE_COMBINER_IFELSEIF, ERROR_LOGLEVEL_INVALID, ERROR_CLIENT_LISTENER, ERROR_CLIENT_CANNOT_GET_READY, ERROR_SYNC_OFFLINE_LOADING, ERROR_STREAMING_SSE, ERROR_STREAMING_AUTH, ERROR_IMPRESSIONS_TRACKER, ERROR_IMPRESSIONS_LISTENER, ERROR_EVENT_TYPE_FORMAT, ERROR_NOT_PLAIN_OBJECT, ERROR_SIZE_EXCEEDED, ERROR_NOT_FINITE, ERROR_CLIENT_DESTROYED, ERROR_NULL, ERROR_TOO_LONG, ERROR_INVALID_KEY_OBJECT, ERROR_INVALID, ERROR_EMPTY, ERROR_EMPTY_ARRAY, ERROR_INVALID_IMPRESSIONS_MODE, ERROR_HTTP, logPrefixSettings, logPrefixEngineCombiner, logPrefixSyncOffline, logPrefixSyncStreaming, logPrefixImpressionsTracker, ERROR_EVENTS_TRACKER, logPrefixEventsTracker } from '../constants';
+import * as c from '../constants';
 
 export const codesError: [number, string][] = [
   // evaluator
-  [ERROR_ENGINE_COMBINER_IFELSEIF, logPrefixEngineCombiner + 'Invalid Split, no valid rules found'],
+  [c.ERROR_ENGINE_COMBINER_IFELSEIF, c.logPrefixEngineCombiner + 'Invalid Split, no valid rules found'],
   // SDK
-  [ERROR_LOGLEVEL_INVALID, 'logger: Invalid Log Level - No changes to the logs will be applied.'],
-  [ERROR_CLIENT_CANNOT_GET_READY, ' The SDK will not get ready. Reason: %s'],
-  [ERROR_IMPRESSIONS_TRACKER, logPrefixImpressionsTracker + 'Could not store impressions bulk with %s impression(s). Error: %s'],
-  [ERROR_IMPRESSIONS_LISTENER, logPrefixImpressionsTracker + 'Impression listener logImpression method threw: %s.'],
-  [ERROR_EVENTS_TRACKER, logPrefixEventsTracker + 'Failed to queue %s'],
+  [c.ERROR_LOGLEVEL_INVALID, 'logger: Invalid Log Level - No changes to the logs will be applied.'],
+  [c.ERROR_CLIENT_CANNOT_GET_READY, ' The SDK will not get ready. Reason: %s'],
+  [c.ERROR_IMPRESSIONS_TRACKER, c.logPrefixImpressionsTracker + 'Could not store impressions bulk with %s impression(s). Error: %s'],
+  [c.ERROR_IMPRESSIONS_LISTENER, c.logPrefixImpressionsTracker + 'Impression listener logImpression method threw: %s.'],
+  [c.ERROR_EVENTS_TRACKER, c.logPrefixEventsTracker + 'Failed to queue %s'],
   // synchronizer
-  [ERROR_SYNC_OFFLINE_LOADING, logPrefixSyncOffline + 'There was an issue loading the mock Splits data, no changes will be applied to the current cache. %s'],
-  [ERROR_STREAMING_SSE, logPrefixSyncStreaming + 'Fail to connect to streaming, with error message: %s'],
-  [ERROR_STREAMING_AUTH, logPrefixSyncStreaming + 'Failed to authenticate for streaming. Error: "%s".'],
-  [ERROR_HTTP, ' Response status is not OK. Status: %s. URL: %s. Message: %s'],
+  [c.ERROR_SYNC_OFFLINE_LOADING, c.logPrefixSyncOffline + 'There was an issue loading the mock Splits data, no changes will be applied to the current cache. %s'],
+  [c.ERROR_STREAMING_SSE, c.logPrefixSyncStreaming + 'Fail to connect to streaming, with error message: %s'],
+  [c.ERROR_STREAMING_AUTH, c.logPrefixSyncStreaming + 'Failed to authenticate for streaming. Error: "%s".'],
+  [c.ERROR_HTTP, ' Response status is not OK. Status: %s. URL: %s. Message: %s'],
   // client status
-  [ERROR_CLIENT_LISTENER, 'A listener was added for %s on the SDK, which has already fired and won\'t be emitted again. The callback won\'t be executed.'],
-  [ERROR_CLIENT_DESTROYED, '%s: Client has already been destroyed - no calls possible.'],
+  [c.ERROR_CLIENT_LISTENER, 'A listener was added for %s on the SDK, which has already fired and won\'t be emitted again. The callback won\'t be executed.'],
+  [c.ERROR_CLIENT_DESTROYED, '%s: Client has already been destroyed - no calls possible.'],
   // input validation
-  [ERROR_EVENT_TYPE_FORMAT, '%s: you passed "%s", event_type must adhere to the regular expression /^[a-zA-Z0-9][-_.:a-zA-Z0-9]{0,79}$/g. This means an event_type must be alphanumeric, cannot be more than 80 characters long, and can only include a dash, underscore, period, or colon as separators of alphanumeric characters.'],
-  [ERROR_NOT_PLAIN_OBJECT, '%s: %s must be a plain object.'],
-  [ERROR_SIZE_EXCEEDED, '%s: the maximum size allowed for the properties is 32768 bytes, which was exceeded. Event not queued.'],
-  [ERROR_NOT_FINITE, '%s: value must be a finite number.'],
-  [ERROR_NULL, '%s: you passed a null or undefined %s. It must be a non-empty string.'],
-  [ERROR_TOO_LONG, '%s: %s too long. It must have 250 characters or less.'],
-  [ERROR_INVALID_KEY_OBJECT, '%s: Key must be an object with bucketingKey and matchingKey with valid string properties.'],
-  [ERROR_INVALID, '%s: you passed an invalid %s. It must be a non-empty string.'],
-  [ERROR_EMPTY, '%s: you passed an empty %s. It must be a non-empty string.'],
-  [ERROR_EMPTY_ARRAY, '%s: %s must be a non-empty array.'],
+  [c.ERROR_EVENT_TYPE_FORMAT, '%s: you passed "%s", event_type must adhere to the regular expression /^[a-zA-Z0-9][-_.:a-zA-Z0-9]{0,79}$/g. This means an event_type must be alphanumeric, cannot be more than 80 characters long, and can only include a dash, underscore, period, or colon as separators of alphanumeric characters.'],
+  [c.ERROR_NOT_PLAIN_OBJECT, '%s: %s must be a plain object.'],
+  [c.ERROR_SIZE_EXCEEDED, '%s: the maximum size allowed for the properties is 32768 bytes, which was exceeded. Event not queued.'],
+  [c.ERROR_NOT_FINITE, '%s: value must be a finite number.'],
+  [c.ERROR_NULL, '%s: you passed a null or undefined %s. It must be a non-empty string.'],
+  [c.ERROR_TOO_LONG, '%s: %s too long. It must have 250 characters or less.'],
+  [c.ERROR_INVALID_KEY_OBJECT, '%s: Key must be an object with bucketingKey and matchingKey with valid string properties.'],
+  [c.ERROR_INVALID, '%s: you passed an invalid %s. It must be a non-empty string.'],
+  [c.ERROR_EMPTY, '%s: you passed an empty %s. It must be a non-empty string.'],
+  [c.ERROR_EMPTY_ARRAY, '%s: %s must be a non-empty array.'],
   // initialization / settings validation
-  [ERROR_INVALID_IMPRESSIONS_MODE, logPrefixSettings + ': you passed an invalid "impressionsMode". It should be one of the following values: %s. Defaulting to "%s" mode.'],
+  [c.ERROR_INVALID_IMPRESSIONS_MODE, c.logPrefixSettings + ': you passed an invalid "impressionsMode". It should be one of the following values: %s. Defaulting to "%s" mode.'],
 ];
--- src/logger/messages/info.ts
+++ src/logger/messages/info.ts
@@ -1,33 +1,33 @@
-import { CLIENT_READY_FROM_CACHE, CLIENT_READY, IMPRESSION, IMPRESSION_QUEUEING, NEW_SHARED_CLIENT, NEW_FACTORY, POLLING_SMART_PAUSING, POLLING_START, POLLING_STOP, SYNC_SPLITS_FETCH_RETRY, STREAMING_REFRESH_TOKEN, STREAMING_RECONNECT, STREAMING_CONNECTING, STREAMING_DISABLED, STREAMING_DISCONNECTING, SUBMITTERS_PUSH_FULL_EVENTS_QUEUE, SUBMITTERS_PUSH, SYNC_START_POLLING, SYNC_CONTINUE_POLLING, SYNC_STOP_POLLING, EVENTS_TRACKER_SUCCESS, logPrefixEventsTracker, logPrefixSyncManager, logPrefixSyncPolling, logPrefixSyncSplits, logPrefixSyncStreaming, logPrefixSyncSubmitters, logPrefixImpressionsTracker, IMPRESSIONS_TRACKER_SUCCESS } from '../constants';
+import * as c from '../constants';
 import { codesWarn } from './warn';
 
 const READY_MSG = 'Split SDK is ready';
 
 export const codesInfo: [number, string][] = codesWarn.concat([
   // client status
-  [CLIENT_READY_FROM_CACHE, READY_MSG + ' from cache'],
-  [CLIENT_READY, READY_MSG],
+  [c.CLIENT_READY_FROM_CACHE, READY_MSG + ' from cache'],
+  [c.CLIENT_READY, READY_MSG],
   // SDK
-  [IMPRESSION, logPrefixImpressionsTracker +'Split: %s. Key: %s. Evaluation: %s. Label: %s'],
-  [IMPRESSION_QUEUEING, logPrefixImpressionsTracker +'Queueing corresponding impression.'],
-  [NEW_SHARED_CLIENT, ' New shared client instance created.'],
-  [NEW_FACTORY, ' New Split SDK instance created.'],
-  [EVENTS_TRACKER_SUCCESS, logPrefixEventsTracker + 'Successfully qeued %s'],
-  [IMPRESSIONS_TRACKER_SUCCESS, logPrefixImpressionsTracker + 'Successfully stored %s impression(s).'],
+  [c.IMPRESSION, c.logPrefixImpressionsTracker +'Split: %s. Key: %s. Evaluation: %s. Label: %s'],
+  [c.IMPRESSION_QUEUEING, c.logPrefixImpressionsTracker +'Queueing corresponding impression.'],
+  [c.NEW_SHARED_CLIENT, ' New shared client instance created.'],
+  [c.NEW_FACTORY, ' New Split SDK instance created.'],
+  [c.EVENTS_TRACKER_SUCCESS, c.logPrefixEventsTracker + 'Successfully qeued %s'],
+  [c.IMPRESSIONS_TRACKER_SUCCESS, c.logPrefixImpressionsTracker + 'Successfully stored %s impression(s).'],
 
   // synchronizer
-  [POLLING_SMART_PAUSING, logPrefixSyncPolling + 'Turning segments data polling %s.'],
-  [POLLING_START, logPrefixSyncPolling + 'Starting polling'],
-  [POLLING_STOP, logPrefixSyncPolling + 'Stopping polling'],
-  [SYNC_SPLITS_FETCH_RETRY, logPrefixSyncSplits + 'Retrying download of splits #%s. Reason: %s'],
-  [SUBMITTERS_PUSH_FULL_EVENTS_QUEUE, logPrefixSyncSubmitters + 'Flushing full events queue and reseting timer.'],
-  [SUBMITTERS_PUSH, logPrefixSyncSubmitters + 'Pushing %s %s.'],
-  [STREAMING_REFRESH_TOKEN, logPrefixSyncStreaming + 'Refreshing streaming token in %s seconds.'],
-  [STREAMING_RECONNECT, logPrefixSyncStreaming + 'Attempting to reconnect in %s seconds.'],
-  [STREAMING_CONNECTING, logPrefixSyncStreaming + 'Connecting to streaming.'],
-  [STREAMING_DISABLED, logPrefixSyncStreaming + 'Streaming is disabled for given Api key. Switching to polling mode.'],
-  [STREAMING_DISCONNECTING, logPrefixSyncStreaming + 'Disconnecting from streaming.'],
-  [SYNC_START_POLLING, logPrefixSyncManager + 'Streaming not available. Starting polling.'],
-  [SYNC_CONTINUE_POLLING, logPrefixSyncManager + 'Streaming couldn\'t connect. Continue polling.'],
-  [SYNC_STOP_POLLING, logPrefixSyncManager + 'Streaming (re)connected. Syncing and stopping polling.'],
+  [c.POLLING_SMART_PAUSING, c.logPrefixSyncPolling + 'Turning segments data polling %s.'],
+  [c.POLLING_START, c.logPrefixSyncPolling + 'Starting polling'],
+  [c.POLLING_STOP, c.logPrefixSyncPolling + 'Stopping polling'],
+  [c.SYNC_SPLITS_FETCH_RETRY, c.logPrefixSyncSplits + 'Retrying download of splits #%s. Reason: %s'],
+  [c.SUBMITTERS_PUSH_FULL_EVENTS_QUEUE, c.logPrefixSyncSubmitters + 'Flushing full events queue and reseting timer.'],
+  [c.SUBMITTERS_PUSH, c.logPrefixSyncSubmitters + 'Pushing %s %s.'],
+  [c.STREAMING_REFRESH_TOKEN, c.logPrefixSyncStreaming + 'Refreshing streaming token in %s seconds.'],
+  [c.STREAMING_RECONNECT, c.logPrefixSyncStreaming + 'Attempting to reconnect in %s seconds.'],
+  [c.STREAMING_CONNECTING, c.logPrefixSyncStreaming + 'Connecting to streaming.'],
+  [c.STREAMING_DISABLED, c.logPrefixSyncStreaming + 'Streaming is disabled for given Api key. Switching to polling mode.'],
+  [c.STREAMING_DISCONNECTING, c.logPrefixSyncStreaming + 'Disconnecting from streaming.'],
+  [c.SYNC_START_POLLING, c.logPrefixSyncManager + 'Streaming not available. Starting polling.'],
+  [c.SYNC_CONTINUE_POLLING, c.logPrefixSyncManager + 'Streaming couldn\'t connect. Continue polling.'],
+  [c.SYNC_STOP_POLLING, c.logPrefixSyncManager + 'Streaming (re)connected. Syncing and stopping polling.'],
 ]);
