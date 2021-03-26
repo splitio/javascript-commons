ea321137cb8199170fb188dbe779b291467eea84
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-03-26 17:05:57 -0300

    comment

--- src/logger/types.ts
+++ src/logger/types.ts
@@ -3,7 +3,7 @@ import { LogLevel } from '../types';
 export interface ILoggerOptions {
   prefix?: string,
   logLevel?: LogLevel,
-  showLevel?: boolean,
+  showLevel?: boolean, // @TODO remove this param eventually since it is not being set `false` anymore
 }
 
 export interface ILogger {
