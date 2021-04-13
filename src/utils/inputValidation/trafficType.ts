df9ad6a4e8e2625902d330185ad192b114ba2ebc
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-04-13 11:48:16 -0300

    fixed some log messages

--- src/utils/inputValidation/trafficType.ts
+++ src/utils/inputValidation/trafficType.ts
@@ -3,15 +3,16 @@ import { ILogger } from '../../logger/types';
 import { isString } from '../lang';
 
 const CAPITAL_LETTERS_REGEX = /[A-Z]/;
+const item = 'traffic_type';
 
 export function validateTrafficType(log: ILogger, maybeTT: any, method: string): string | false {
   if (maybeTT == undefined) { // eslint-disable-line eqeqeq
-    log.error(ERROR_NULL, [method]);
+    log.error(ERROR_NULL, [method, item]);
   } else if (!isString(maybeTT)) {
-    log.error(ERROR_INVALID, [method]);
+    log.error(ERROR_INVALID, [method, item]);
   } else {
     if (maybeTT.length === 0) {
-      log.error(ERROR_EMPTY, [method]);
+      log.error(ERROR_EMPTY, [method, item]);
     } else {
       if (CAPITAL_LETTERS_REGEX.test(maybeTT)) {
         log.warn(WARN_LOWERCASE_TRAFFIC_TYPE, [method]);
