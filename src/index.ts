d4343fa9669262816e336cb1474e03549ebdcec0
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-05-04 16:41:41 -0300

    updated index

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,5 @@ export { InMemoryStorageFactory } from './storages/inMemory/InMemoryStorage';
 // InMemory storage factory for standalone client-side SplitFactory
 export { InMemoryStorageCSFactory } from './storages/inMemory/InMemoryStorageCS';
 
-// InMemory caches, reused to implement new storages
-export { default as SplitsCacheInMemory } from './storages/inMemory/SplitsCacheInMemory';
-export { default as MySegmentsCacheInMemory } from './storages/inMemory/MySegmentsCacheInMemory';
-export { default as SegmentsCacheInMemory } from './storages/inMemory/SegmentsCacheInMemory';
-export { default as EventsCacheInMemory } from './storages/inMemory/EventsCacheInMemory';
-export { default as ImpressionsCacheInMemory } from './storages/inMemory/ImpressionsCacheInMemory';
-export { default as LatenciesCacheInMemory } from './storages/inMemory/LatenciesCacheInMemory';
-export { default as CountsCacheInMemory } from './storages/inMemory/CountsCacheInMemory';
-
-// Utils for implementing new storages for standalone SplitFactory
-export { default as AbstractSplitsCacheSync, usesSegments } from './storages/AbstractSplitsCacheSync';
-export { default as AbstractSegmentsCacheSync } from './storages/AbstractSegmentsCacheSync';
-
-// Evaluator:
-export { evaluateFeature, evaluateFeatures } from './evaluator';
-
-// Utils:
-export { hash128 } from './utils/murmur3/murmur3_128';
-export { hash } from './utils/murmur3/murmur3';
+// Pluggable storage factory for consumer/producer mode
+export { PluggableStorage } from './storages/pluggable';
