a57d9f2bcfb63cd7656100f00540fc387eedff3b
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-05-18 18:53:59 -0300

    updated manager to handle errors on pluggable storage calls

--- src/sdkManager/__tests__/index.asyncCache.spec.ts
+++ src/sdkManager/__tests__/index.asyncCache.spec.ts
@@ -3,6 +3,8 @@ import splitObject from './mocks/input.json';
 import splitView from './mocks/output.json';
 import { sdkManagerFactory } from '../index';
 import SplitsCacheInRedis from '../../storages/inRedis/SplitsCacheInRedis';
+import { SplitsCachePluggable } from '../../storages/pluggable/SplitsCachePluggable';
+import { wrapperAdapter } from '../../storages/pluggable/wrapperAdapter';
 import KeyBuilderSS from '../../storages/KeyBuilderSS';
 import { ISdkReadinessManager } from '../../readiness/types';
 import { loggerMock } from '../../logger/__tests__/sdkLogger.mock';
@@ -16,48 +18,71 @@ const sdkReadinessManagerMock = {
   sdkStatus: jest.fn()
 } as ISdkReadinessManager;
 
-test('MANAGER API / Async cache (In Redis)', async () => {
+// @ts-expect-error
+const keys = new KeyBuilderSS();
 
-  /** Setup: create manager */
-  const connection = new Redis({}); // @ts-expect-error
-  const keys = new KeyBuilderSS();
-  const cache = new SplitsCacheInRedis(loggerMock, keys, connection);
-  const manager = sdkManagerFactory(loggerMock, cache, sdkReadinessManagerMock);
-  await cache.clear();
-  await cache.addSplit(splitObject.name, JSON.stringify(splitObject));
+describe('MANAGER API', () => {
 
-  /** List all splits */
-  const views = await manager.splits();
-  expect(views[0]).toEqual(splitView);
+  afterEach(() => { loggerMock.mockClear(); });
 
-  /** Read only one split by name */
+  test('Async cache (In Redis)', async () => {
 
-  const split = await manager.split(splitObject.name);
-  expect(split).toEqual(splitView);
+    /** Setup: create manager */
+    const connection = new Redis({});
+    const cache = new SplitsCacheInRedis(loggerMock, keys, connection);
+    const manager = sdkManagerFactory(loggerMock, cache, sdkReadinessManagerMock);
+    await cache.clear();
+    await cache.addSplit(splitObject.name, JSON.stringify(splitObject));
+
+    /** List all splits */
+    const views = await manager.splits();
+    expect(views[0]).toEqual(splitView);
+
+    /** Read only one split by name */
+
+    const split = await manager.split(splitObject.name);
+    expect(split).toEqual(splitView);
 
 
-  /** List all the split names */
+    /** List all the split names */
 
-  const names = await manager.names();
-  expect(names.indexOf(splitObject.name) !== -1).toBe(true);
+    const names = await manager.names();
+    expect(names.indexOf(splitObject.name) !== -1).toBe(true);
 
-  /** Input Validation */
+    /** Input Validation */
 
-  // control assertions to verify that the manager is connected with that cache.
-  expect((await manager.split(splitObject.name)) != null).toBe(true); // control assertion for split.
-  expect((await manager.splits()).length > 0).toBe(true); // control assertion for splits.
-  expect((await manager.names()).length > 0).toBe(true); // control assertion for names.
+    // control assertions to verify that the manager is connected with that cache.
+    expect((await manager.split(splitObject.name)) != null).toBe(true); // control assertion for split.
+    expect((await manager.splits()).length > 0).toBe(true); // control assertion for splits.
+    expect((await manager.names()).length > 0).toBe(true); // control assertion for names.
 
-  // @ts-expect-error
-  expect(await manager.split(undefined)).toBe(null); // If the split name is invalid, `manager.split(invalidName)` returns null.
+    // @ts-expect-error
+    expect(await manager.split(undefined)).toBe(null); // If the split name is invalid, `manager.split(invalidName)` returns null.
 
-  // This is kind of tied to the implementation of the isOperational validator.
-  (sdkReadinessManagerMock.readinessManager.isDestroyed as jest.Mock).mockImplementation(() => true);
+    // This is kind of tied to the implementation of the isOperational validator.
+    (sdkReadinessManagerMock.readinessManager.isDestroyed as jest.Mock).mockImplementation(() => true);
 
-  expect(await manager.split(splitObject.name)).toBe(null); // If the factory/client is destroyed, `manager.split(validName)` will return null either way since the storage is not valid.
-  expect(await manager.splits()).toEqual([]); // If the factory/client is destroyed, `manager.splits()` will return empty array either way since the storage is not valid.
-  expect(await manager.names()).toEqual([]); // If the factory/client is destroyed, `manager.names()` will return empty array either way since the storage is not valid.
+    expect(await manager.split(splitObject.name)).toBe(null); // If the factory/client is destroyed, `manager.split(validName)` will return null either way since the storage is not valid.
+    expect(await manager.splits()).toEqual([]); // If the factory/client is destroyed, `manager.splits()` will return empty array either way since the storage is not valid.
+    expect(await manager.names()).toEqual([]); // If the factory/client is destroyed, `manager.names()` will return empty array either way since the storage is not valid.
 
-  /** Teardown */
-  await connection.quit();
+
+
+    /** Teardown */
+    await connection.quit();
+  });
+
+  test('Async cache with error', async () => {
+    // passing an empty object as wrapper, to make method calls of splits cache fail returning a rejected promise.
+    // @ts-expect-error
+    const cache = new SplitsCachePluggable(loggerMock, keys, wrapperAdapter(loggerMock, {}));
+    const manager = sdkManagerFactory(loggerMock, cache, sdkReadinessManagerMock);
+
+    expect(await manager.split('some_spplit')).toEqual(null);
+    expect(await manager.splits()).toEqual([]);
+    expect(await manager.names()).toEqual([]);
+
+    expect(loggerMock.error).toBeCalledTimes(3); // 3 error logs, one for each attempt to call a wrapper method
+  });
+
 });
