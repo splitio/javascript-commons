f68fe6b49c576940207db6733919e5ec85b10ab8
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-12-01 15:27:48 -0300

    upgraded ioredis for vulnerability fix

--- src/utils/env/isNode.ts
+++ src/utils/env/isNode.ts
@@ -1,3 +1,6 @@
-// We check for version truthiness since most shims will have that as empty string.
 // eslint-disable-next-line no-undef
+/**
+ * 'true' if running in Node.js, or 'false' otherwise.
+ * We check for version truthiness since most shims will have that as empty string.
+ */
 export const isNode: boolean = typeof process !== 'undefined' && typeof process.version !== 'undefined' && !!process.version ? true : false;
