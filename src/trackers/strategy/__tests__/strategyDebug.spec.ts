b26163b96d2727f7ebb32c5aba90c746aa8a9846
Emmanuel Zamora <emmanuel-zamora@split.io>
2022-07-19 20:25:28 -0300

    [SDKS-5959] Add None strategy. improve testUtils usage

--- src/trackers/strategy/__tests__/strategyDebug.spec.ts
+++ src/trackers/strategy/__tests__/strategyDebug.spec.ts
@@ -1,15 +1,15 @@
 import { impressionObserverSSFactory } from '../../impressionObserver/impressionObserverSS';
 import { impressionObserverCSFactory } from '../../impressionObserver/impressionObserverCS';
 import { strategyDebugFactory } from '../strategyDebug';
-import { impression1, impression12, impression2, processStrategy } from './testUtils';
+import { impression1, impression2, processStrategy } from './testUtils';
 
 test('strategyDebug', () => {
   
   let augmentedImp1 = { ...impression1, pt: undefined };
-  let augmentedImp12 = { ...impression12, pt: impression1.time };
+  let augmentedImp12 = { ...impression1, pt: impression1.time };
   let augmentedImp2 = { ...impression2, pt: undefined };
   
-  let impressions = [{...impression1}, {...impression2}, {...impression12}];
+  let impressions = [impression1, impression2, {...impression1}];
   let augmentedImpressions = [augmentedImp1, augmentedImp2, augmentedImp12];
   
   const strategyDebugSS = strategyDebugFactory(impressionObserverSSFactory());
