b8edfeddb1e858ef667d171603c012e2318c5541
Emiliano Sanchez <emiliano.sanchez@split.io>
2022-07-21 17:14:07 -0300

    update autoRequire script for readability

--- src/integrations/ga/autoRequire.js
+++ src/integrations/ga/autoRequire.js
@@ -2,31 +2,31 @@
 /**
  * Auto-require script to use with GoogleAnalyticsToSplit integration
  */
-(function (i, r, s) {
-  i[s] = i[s] || r;
-  i[r] = i[r] || function () { i[r].q.push(arguments); };
-  i[r].q = i[r].q || [];
+(function (w, g, o) {
+  w[o] = w[o] || g;
+  w[g] = w[g] || function () { w[g].q.push(arguments); };
+  w[g].q = w[g].q || [];
 
-  var ts = {}; // Tracker names
+  var trackerNames = {};
   function name(arg) { return typeof arg === 'object' && typeof arg.name === 'string' && arg.name; }
 
-  function processCommand(v) { // Queue a `require` command if v is a `create` command
-    if (v && v[0] === 'create') {
-      var t = name(v[1]) || name(v[2]) || name(v[3]) || (typeof v[3] === 'string' ? v[3] : undefined); // Get tracker name
+  function processCommand(command) { // Queue a `require` command if v is a `create` command
+    if (command && command[0] === 'create') {
+      var trackerName = name(command[1]) || name(command[2]) || name(command[3]) || (typeof command[3] === 'string' ? command[3] : undefined); // Get tracker name
 
-      if (!ts[t]) {
-        ts[t] = true;
-        i[r]((t ? t + '.' : '') + 'require', 'splitTracker'); // Auto-require
+      if (!trackerNames[trackerName]) {
+        trackerNames[trackerName] = true;
+        w[g]((trackerName ? trackerName + '.' : '') + 'require', 'splitTracker'); // Auto-require
       }
     }
   }
 
-  i[r].q.forEach(processCommand); // Process already queued commands
+  w[g].q.forEach(processCommand); // Process already queued commands
 
-  var o = i[r].q.push;
-  i[r].q.push = function (v) { // Spy new queued commands
-    var result = o.apply(this, arguments);
-    processCommand(v);
+  var originalPush = w[g].q.push;
+  w[g].q.push = function (command) { // Spy new queued commands
+    var result = originalPush.apply(this, arguments);
+    processCommand(command);
     return result;
   };
 
