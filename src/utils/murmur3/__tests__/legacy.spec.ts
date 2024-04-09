b9be65c209da08daffcd9b1da11a5379cc923ad1
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-04-09 15:13:09 -0300

    Code format update

--- src/utils/murmur3/__tests__/legacy.spec.ts
+++ src/utils/murmur3/__tests__/legacy.spec.ts
@@ -3,10 +3,9 @@ import * as utils from '../legacy';
 import { readCSV } from '../../../__tests__/testUtils/csv';
 
 test('ENGINE / validate hashing behavior using sample data', async () => {
-  const data = await readCSV(require.resolve('./mocks/small-data.csv'));
+  const lines = await readCSV(require.resolve('./mocks/small-data.csv'));
 
-  for (let [seed, key, hash, bucket] of data) {
-
+  for (let [seed, key, hash, bucket] of lines) {
     seed = parseInt(seed, 10);
     hash = parseInt(hash, 10);
     bucket = parseInt(bucket, 10);
@@ -14,5 +13,4 @@ test('ENGINE / validate hashing behavior using sample data', async () => {
     expect(utils.hash(key, seed)).toBe(hash); // matching using int32 hash value
     expect(utils.bucket(key, seed)).toBe(bucket); // matching using int32 bucket value
   }
-
 });
