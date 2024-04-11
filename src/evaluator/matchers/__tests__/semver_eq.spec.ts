bc28b345479a2a76a94030257c596adcc4ec8ce3
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-04-11 12:50:44 -0300

    Polishing

--- src/evaluator/matchers/__tests__/semver_eq.spec.ts
+++ src/evaluator/matchers/__tests__/semver_eq.spec.ts
@@ -5,9 +5,9 @@ import { loggerMock } from '../../../logger/__tests__/sdkLogger.mock';
 import { readCSV } from '../../../__tests__/testUtils/csv';
 
 test('MATCHER EQUAL TO SEMVER', async () => {
-  const data = await readCSV(require.resolve('./mocks/equal-to-semver.csv'));
+  const lines = await readCSV(require.resolve('./mocks/equal-to-semver.csv'));
 
-  for (const [ruleSemver, runtimeSemver, result] of data) {
+  for (const [ruleSemver, runtimeSemver, result] of lines) {
     let matcher = matcherFactory(loggerMock, {
       negate: false,
       type: matcherTypes.EQUAL_TO_SEMVER,
