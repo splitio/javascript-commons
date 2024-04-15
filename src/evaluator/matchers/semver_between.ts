276dc00c3c21eccb523c40594dfdeb72097ed93c
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-04-15 13:54:56 -0300

    Style formatting

--- src/evaluator/matchers/semver_between.ts
+++ src/evaluator/matchers/semver_between.ts
@@ -3,15 +3,13 @@ import { ILogger } from '../../logger/types';
 import { Semver } from '../../utils/Semver';
 
 export function betweenSemverMatcherContext(log: ILogger, ruleAttr: IBetweenStringMatcherData) {
-
   const startSemver = new Semver(ruleAttr.start);
   const endSemver = new Semver(ruleAttr.end);
 
   return function betweenSemverMatcher(key: string): boolean {
-
     const runtimeSemver = new Semver(key);
 
-    let isBetween = startSemver.compare(runtimeSemver) <= 0 && endSemver.compare(runtimeSemver) >= 0;
+    const isBetween = startSemver.compare(runtimeSemver) <= 0 && endSemver.compare(runtimeSemver) >= 0;
 
     return isBetween;
   };
