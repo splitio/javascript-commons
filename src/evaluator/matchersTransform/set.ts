e1edc7993b3d684a57e21c36152b3908bcbd3427
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-04-17 12:16:30 -0300

    Updated whitelist and in_list_semver matchers to reuse matchersTransform logic

--- src/evaluator/matchersTransform/set.ts
+++
@@ -1,8 +0,0 @@
-import { ISplitMatcher } from '../../dtos/types';
-
-/**
- * Extract whitelist array. Used by set and string matchers
- */
-export function setTransform(whitelistObject: ISplitMatcher['whitelistMatcherData']) {
-  return whitelistObject && whitelistObject.whitelist;
-}
