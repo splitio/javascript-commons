ea68f1537eae104c9752e04956e66225ea6e4fd0
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-05-27 15:33:38 -0300

    Polish tests

--- src/sdkClient/__tests__/clientInputValidation.spec.ts
+++ src/sdkClient/__tests__/clientInputValidation.spec.ts
@@ -28,14 +28,28 @@ describe('clientInputValidationDecorator', () => {
     expect(clientWithValidation.getTreatment('key')).toEqual('control');
     expect(logSpy).toHaveBeenLastCalledWith('[ERROR] splitio => getTreatment: you passed a null or undefined feature flag name. It must be a non-empty string.');
 
-    expect(clientWithValidation.getTreatmentsWithConfig('key')).toEqual({});
+    expect(clientWithValidation.getTreatmentWithConfig('key', [])).toEqual({ treatment: 'control', config: null });
+    expect(logSpy).toHaveBeenLastCalledWith('[ERROR] splitio => getTreatmentWithConfig: you passed an invalid feature flag name. It must be a non-empty string.');
+
+    expect(clientWithValidation.getTreatments('key')).toEqual({});
+    expect(logSpy).toHaveBeenLastCalledWith('[ERROR] splitio => getTreatments: feature flag names must be a non-empty array.');
+
+    expect(clientWithValidation.getTreatmentsWithConfig('key', [])).toEqual({});
     expect(logSpy).toHaveBeenLastCalledWith('[ERROR] splitio => getTreatmentsWithConfig: feature flag names must be a non-empty array.');
 
-    // @TODO fix log error message for getTreatmentsByFlagSet
-    // expect(clientWithValidation.getTreatmentsByFlagSet('key')).toEqual({});
-    // expect(logSpy).toHaveBeenLastCalledWith('[ERROR] splitio => getTreatmentsByFlagSet: you passed a null or undefined flag set name. It must be a non-empty string.');
+    expect(clientWithValidation.getTreatmentsByFlagSet('key')).toEqual({});
+    expect(logSpy).toBeCalledWith('[ERROR] splitio => getTreatmentsByFlagSet: you passed a null or undefined flag set. It must be a non-empty string.');
 
-    expect(clientWithValidation.getTreatmentsWithConfigByFlagSets('key')).toEqual({});
+    expect(clientWithValidation.getTreatmentsWithConfigByFlagSet('key', [])).toEqual({});
+    expect(logSpy).toBeCalledWith('[ERROR] splitio => getTreatmentsWithConfigByFlagSet: you passed an invalid flag set. It must be a non-empty string.');
+
+    expect(clientWithValidation.getTreatmentsByFlagSets('key')).toEqual({});
+    expect(logSpy).toHaveBeenLastCalledWith('[ERROR] splitio => getTreatmentsByFlagSets: flag sets must be a non-empty array.');
+
+    expect(clientWithValidation.getTreatmentsWithConfigByFlagSets('key', [])).toEqual({});
     expect(logSpy).toHaveBeenLastCalledWith('[ERROR] splitio => getTreatmentsWithConfigByFlagSets: flag sets must be a non-empty array.');
+
+    // @TODO should be 8, but there is an additional log from `getTreatmentsByFlagSet` and `getTreatmentsWithConfigByFlagSet` that should be removed
+    expect(logSpy).toBeCalledTimes(10);
   });
 });
