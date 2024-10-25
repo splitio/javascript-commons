35953b753b08df2a0b64bf0bd7f51b50283491e0
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-10-25 12:40:00 -0300

    1st step: move SplitIO namespace definition from Browser SDK to JS-Commons

---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+// Declaration file for JavaScript Browser Split Software SDK
+// Project: http://www.split.io/
+// Definitions by: Nico Zelaya <https://github.com/NicoZelaya/>
+
+/// <reference path="./splitio.d.ts" />
